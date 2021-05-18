08734f305903a9e08f7ec354992979e140509123
itpythongit2 <34918569+Erichile@users.noreply.github.com>
2021-05-19 03:01:37 +0800

    gitee commit

--- src/main/java/com/zixue/git/GitTest.java
+++ src/main/java/com/zixue/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("master2 git");
         System.out.println("hot-fix2 git");
         System.out.println("pull test");
+        System.out.println("gitee test");
     }
 }
